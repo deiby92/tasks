bcb97283f4b9e6ca8387f1bbde39c06dc6110d83
unknown <deivy-9208@hotmail.com>
2024-10-17 16:49:15 -0500

    first commit

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 from datetime import timedelta
 import os
-
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -10,13 +10,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m%irmz+4ml(@%oi+9#u_0!a+#v21p6p)^s@(r9xjz3--910j%o'
+SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = []
-
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
@@ -43,6 +45,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.meddleware.whiteNoiseMiddleware',
 ]
 
 MIDDLEWARE.remove('django.middleware.csrf.CsrfViewMiddleware') #Evitar problemas con Csrf de django
@@ -101,10 +104,13 @@ WSGI_APPLICATION = 'proyecto.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='postgresql://postgresql:postgresql@localhost/postgresql',
+        conn_max_age=600
+
+    )
+    
+      
 }
 
 
@@ -144,6 +150,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+    STATICFILES_STORAGE ='whitenoise.storage.compressedMainfestStaticFilesStorage'
 
 LOGIN_URL = 'signin'
 # Default primary key field type
